8fec6dfd2efd53f07917133c1a8bcfa391af1163
Иван Шваб <118583334+edvaldovic@users.noreply.github.com>
2023-04-09 10:06:07 +0700

    подключение плавного передвижения по секциям через js  вмести с бургиром

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const burgerPopup = document.querySelector('.burger__popup')
 const burgerLineFirst = burgerMenu.querySelector('.burger__line__first');
 const burgerLineSecond = burgerMenu.querySelector('.burger__line__second');
 const burgerLineThir = burgerMenu.querySelector('.burger__line__thir');
+const portfolioLink = document.querySelector('.link__portfolio');
+const portfolioLinkBurger = document.querySelector('.link__portfolio__burger');
+const portolioSection = document.querySelector('#portfolio');
+const aboutLink = document.querySelector('.link__about');
+const aboutLinkBurger = document.querySelector('.link__about__burger');
+const aboutSection = document.querySelector('#about');
+const contactLink = document.querySelector('.link__contact');
+const contactLinkBurger = document.querySelector('.link__contact__burger');
+const contactSection = document.querySelector('#contact');
+const homeLinkBurger = document.querySelector('.link__home__burger');
+const homeSectionBurger = document.querySelector('#home');
 
 
 function togglburgerMenu() {
@@ -26,4 +37,39 @@ burgerMenu.addEventListener('click', () => {
 burgerPopup.addEventListener('click', () => {
   togglburgerPopup();
   togglburgerMenu();
+})
+
+portfolioLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  portolioSection.scrollIntoView({ behavior: "smooth", block: "start" });
+})
+
+portfolioLinkBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  portolioSection.scrollIntoView({ behavior: "smooth", block: "start" });
+})
+
+aboutLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutSection.scrollIntoView({ behavior: "smooth", block: "start" });
+})
+
+aboutLinkBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutSection.scrollIntoView({ behavior: "smooth", block: "start" });
+})
+
+contactLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  contactSection.scrollIntoView({ behavior: "smooth", block: "start" });
+})
+
+contactLinkBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  contactSection.scrollIntoView({ behavior: "smooth", block: "start" });
+})
+
+homeLinkBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  homeSectionBurger.scrollIntoView({ behavior: "smooth", block: "start" });
 })
