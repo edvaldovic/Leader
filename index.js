3cc5992c2dc7b32aa0d9f1cb6219d9de5e748e78
Иван Шваб <118583334+edvaldovic@users.noreply.github.com>
2023-04-16 11:46:10 +0700

    добавление фоток

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const burgerMenu = document.querySelector('.burger')
-const burgerPopup = document.querySelector('.burger__popup')
+const burgerMenu = document.querySelector('.burger');
+const burgerPopup = document.querySelector('.burger__popup');
 const burgerLineFirst = burgerMenu.querySelector('.burger__line__first');
 const burgerLineSecond = burgerMenu.querySelector('.burger__line__second');
 const burgerLineThir = burgerMenu.querySelector('.burger__line__thir');
@@ -14,30 +14,98 @@ const contactLinkBurger = document.querySelector('.link__contact__burger');
 const contactSection = document.querySelector('#contact');
 const homeLinkBurger = document.querySelector('.link__home__burger');
 const homeSectionBurger = document.querySelector('#home');
+const buttonShu = document.querySelector('.button__shu');
+const puerShu = document.querySelectorAll('.shu');
+const whole = document.querySelectorAll('.image');
+const buttonShen = document.querySelector('.button__shen');
+const puerShen = document.querySelectorAll('.shen');
+const buttonPao = document.querySelector('.button__pao');
+const hunPao = document.querySelectorAll('.pao');
+const buttonYin = document.querySelector('.button__yin');
+const guanYin = document.querySelector('.yin')
 
 
-function togglburgerMenu() {
+
+function toggleburgerMenu() {
   burgerLineFirst.classList.toggle('line_first_opened');
   burgerLineSecond.classList.toggle('line_second_opened');
   burgerLineThir.classList.toggle('line_thir_opened');
 }
 
 
-function togglburgerPopup() {
+function toggleburgerPopup() {
   burgerPopup.classList.toggle('burger__popup__opened');
 }
 
 
 burgerMenu.addEventListener('click', () => {
-  togglburgerMenu();
-  togglburgerPopup();
+  toggleburgerMenu();
+  toggleburgerPopup();
 });
 
 
 burgerPopup.addEventListener('click', () => {
-  togglburgerPopup();
-  togglburgerMenu();
-})
+  toggleburgerPopup();
+  toggleburgerMenu();
+});
+
+
+function toggleClosePhoto() {
+  whole.forEach((item) => item.classList.toggle('shu'))
+}
+
+function toggleOpenPhoto() {
+  puerShu.forEach((item) => item.classList.toggle('open'))
+}
+
+buttonShu.addEventListener('click', () => {
+  toggleClosePhoto();
+  toggleOpenPhoto();
+});
+
+
+function ClosePhotoShen() {
+  whole.forEach((item) => item.classList.toggle('shen'))
+}
+
+function toggleOpenPhotoShen() {
+  puerShen.forEach((item) => item.classList.toggle('open'))
+}
+
+buttonShen.addEventListener('click', () => {
+  ClosePhotoShen();
+  toggleOpenPhotoShen();
+});
+
+
+function toggleClosePhotoPao() {
+  whole.forEach((item) => item.classList.toggle('pao'))
+}
+
+function toggleOpenPhotoPao() {
+  hunPao.forEach((item) => item.classList.toggle('open'))
+}
+
+buttonPao.addEventListener('click', () => {
+  toggleClosePhotoPao();
+  toggleOpenPhotoPao();
+});
+
+
+function toggleClosePhotoYin() {
+  whole.forEach((item) => item.classList.toggle('yin'))
+}
+
+function toggleOpenPhotoYin() {
+  guanYin.forEach((item) => item.classList.toggle('open'))
+}
+
+buttonYin.addEventListener('click', () => {
+  toggleClosePhotoYin();
+  toggleOpenPhotoYin();
+});
+
+
 
 portfolioLink.addEventListener('click', (e) => {
   e.preventDefault();
